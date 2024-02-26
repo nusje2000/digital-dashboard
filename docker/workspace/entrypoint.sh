6f37335bab5c88f35565468cf5a09084d06517ec
Maarten Nusteling <maarten.nusteling@gmail.com>
2024-02-26 17:16:22 +0100

    Improved setup script

--- docker/workspace/entrypoint.sh
+++ docker/workspace/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-bun install --frozen-lockfile
+if ! sha1sum -c install.lock &> /dev/null; then
+    echo "Invalid install.lock, updating dependencies."
+
+    rm -rf ./*/*/node_modules
+    rm -rf ./node_modules
+
+    bun install --frozen-lockfile
+    sha1sum bun.lockb > install.lock
+else
+    echo "Valid install.lock, skipping dependency update."
+fi
 
 tail -f /dev/null
